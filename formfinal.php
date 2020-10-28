d1a6ca7e08806f121dd5f946afbad050535675b6
aarsh121 <51901455+aarsh121@users.noreply.github.com>
2020-10-28 15:14:38 +0530

    Create formfinal.php
    Final form for SIgnup. It contain first_name, Last_name, Email field and a button to see all the entries. It's CSS file name is Style.css.

---
+++ formfinal.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<h1>
+Registration Form
+</h1>
+<form action="welcome.php" method="POST" id="nav">
+First Name: <input type="text" name="fname" required><br>
+Last Name: <input type="text" name="lname" required><br>
+E-mail: <input type="text" name="email" required><br>
+<input type="submit" placeholder="Submit">
+</form>
+<button type="button" style="text-color:white; padding:15px 32px; text-align:center;background-color:#4CAF50; text-decoration:none; display:inline-block;>font-size:16px;margin-left:500px;margin-bottom:100px;"><a href="validate.html">Click here to view the list</a></button>
+
+
+</body>
+</html>
